de7ffe7b43749c59eaca4d43e85c519b4859619b
joufando <joufando@gmail.com>
2024-07-21 12:15:23 +0700

    feat particle

---
+++ client/components/Hero.tsx
@@ -0,0 +1,10 @@
+import React from "react"
+import Particle from "./Particle"
+
+export default function Hero() {
+    return (
+        <div className="h-[88vh] bg-[url('/images/banner.jpg')] bg-cover bg-center">
+            <Particle />
+        </div>
+    )
+}
---
+++ client/components/Nav.tsx
@@ -0,0 +1,27 @@
+import React from "react"
+import { Bars3Icon } from "@heroicons/react/20/solid"
+
+interface Props {
+    openNav: () => void;
+}
+
+export default function Nav({ openNav }: Props) {
+    return (
+        <div className="w-[100%] top-0 h-[12vh] bg-[#141c27] shadow-md">
+            <div className="flex items-center justify-between w-[80%] mx-auto h-[100%]">
+                <h1 className="flex-[0.6] cursor-pointer text-[25px] text-white font-bold">
+                    JOU
+                    <span className="text-cyan-500">FANDO</span>
+                </h1>
+                <div className="nav-link">HOME</div>
+                <div className="nav-link">SERVICES</div>
+                <div className="nav-link">ABOUT</div>
+                <div className="nav-link">PROJECT</div>
+                <div className="nav-link">CONTACT</div>
+                <div onClick={openNav}>
+                    <Bars3Icon className="w-[2rem] md:hidden text-cyan-500 cursor-pointer" />
+                </div>
+            </div>
+        </div>
+    )
+}
