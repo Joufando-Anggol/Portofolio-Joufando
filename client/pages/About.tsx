564ef938c280a72cd70388ef26263c3b95b235b8
joufando <joufando@gmail.com>
2024-07-27 00:38:39 +0700

    changed aboutme pic

--- client/pages/About.tsx
+++ client/pages/About.tsx
@@ -11,10 +11,11 @@ export default function About() {
             <div className="grid grid-cols-1 md:grid-cols-2 w-11/12 lg:w-4/5 mx-auto gap-12 items-center">
                 {/* Text Section */}
                 <div className="flex flex-col justify-center items-center text-center md:text-left">
-                    <h2 className=" heading text-2xl md:text-4xl lg:text-5xl leading-snug md:leading-normal font-bold text-white mb-12">
+                    <h2 className="heading text-2xl md:text-4xl lg:text-5xl leading-snug md:leading-normal font-bold text-white mb-12">
                         ABOUT <span className="text-cyan-500">ME</span>
                     </h2>
                     <p className="text-base md:text-lg text-[#ffffff92] mb-6">
+                        Bootcamp graduate skilled in frontend (HTML, CSS, JavaScript, React, Next.js) and backend (Node.js, Express.js, MongoDB, PostgreSQL) development. Proficient in React Native for mobile apps. Experienced in Git, CI/CD, and cloud deployments.
                         For more details about my qualifications and experience, please refer to my CV, Transcript, or Certificate by clicking the button below.
                     </p>
                     <div className="flex flex-col space-y-4">
@@ -46,14 +47,14 @@ export default function About() {
                 </div>
                 {/* Image Section */}
                 <div data-aos="fade-left" className="relative lg:w-[300px] mx-auto md:mx-0 mt-8 lg:mt-0 lg:h-[300px] w-[250px] h-[250px]">
+                    <div className="absolute inset-0 bg-[#55e6a5] z-0 transform translate-x-4 translate-y-4"></div>
                     <Image
-                        src="/images/aboutme.jpg"
+                        src="https://img.freepik.com/free-vector/man-shows-gesture-great-idea_10045-637.jpg?ga=GA1.1.780443899.1721550221&semt=ais_user"
                         alt="user"
                         layout="fill"
                         objectFit="contain"
-                        className="relative z-10 w-full h-full object-contain"
+                        className="relative z-10"
                     />
-                    <div className="absolute inset-0 bg-[#55e6a5] z-0 transform -translate-x-4 -translate-y-4"></div>
                 </div>
             </div>
         </div>
